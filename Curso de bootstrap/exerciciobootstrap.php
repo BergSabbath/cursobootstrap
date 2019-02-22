c54d903ed92d346745212273ea6bc17154457a94
BergSabbath <bergit7@gmail.com>
2019-02-22 16:56:02 -0300

    treinando o bootstrap

---
+++ Curso de bootstrap/exerciciobootstrap.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Exercicio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css" />
+</head>
+<body>
+<style>
+
+div{
+    border: 2px solid black;
+    text-align: justify;
+}
+.col-lg-3{
+    font-size: 18pt;
+}
+</style>
+    <section class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Questão bootstrap</h1>
+        </div>
+    </section>
+    <section class="container">
+        <div class="row">
+            <div class="col-lg-2 col-md-4">Propaganda A</div>
+            <div class="col-lg-8 col-md-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum semper nulla at consequat mollis. Sed euismod nisi vitae dictum aliquam. Ut vel tortor nec nisl vestibulum tempor nec ut tortor. Vestibulum vestibulum lacus sit amet accumsan laoreet. Donec pellentesque ex lectus, et facilisis justo interdum ullamcorper. Mauris gravida eget orci et vulputate. Nam dapibus, magna vel volutpat imperdiet, magna lectus auctor neque, in hendrerit ex ipsum eget est. Nam eleifend semper vestibulum. Donec elit orci, sollicitudin in nisi at, facilisis fringilla lectus. Vivamus ut vulputate justo. Nam eget pretium lorem.
+
+            </div>
+            <div class="col-lg-2">Propaganda B</div>
+        </div>
+        <div class="row">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum semper nulla at consequat mollis. Sed euismod nisi vitae dictum aliquam. Ut vel tortor nec nisl vestibulum tempor nec ut tortor. Vestibulum vestibulum lacus sit amet accumsan laoreet. Donec pellentesque ex lectus, et facilisis justo interdum ullamcorper. Mauris gravida eget orci et vulputate. Nam dapibus, magna vel volutpat imperdiet, magna lectus auctor neque, in hendrerit ex ipsum eget est. Nam eleifend semper vestibulum. Donec elit orci, sollicitudin in nisi at, facilisis fringilla lectus. Vivamus ut vulputate justo. Nam eget pretium lorem.
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-3">1</div>
+            <div class="col-lg-3 col-md-3 col-sm-3">2</div>
+            <div class="col-lg-3 col-md-3 col-sm-3">3</div>
+            <div class="col-lg-3 col-md-3 col-sm-3">4</div>
+        </div>
+    </section>
+
+    <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+</body>
+</html>
---
+++ Curso de bootstrap/sistemagrid.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css">
+    <style>
+        div{
+            border: 1px solid black;
+        }
+        .container{
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <section class="container">
+
+        <div class="row">
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+
+
+        </div>
+        <div class="row">
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+            <div class="col bg-info">coluna</div>
+
+        </div>
+        <div class="row">
+            <div class="col-sm-8 bg-success">colunas</div>
+            <div class="col-sm-4 bg-danger">colunas</div>
+        </div>
+
+    </section>       
+
+    <script src="node_modules\bootstrap\dist\js\css\bootstrap.min.js"></script>
+</body>
+</html>
